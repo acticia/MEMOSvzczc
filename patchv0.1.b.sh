230bfff45bef92cf441fd6d29293ba8dc0a87b16
acticia <adm-tsa@lagrange.acticia.net>
2014-09-15 11:43:31 +0200

    Create patchv0.1.b.sh

---
+++ patchv0.1.b.sh
@@ -0,0 +1,3 @@
+/bin/echo deb http://192.168.0.199:9999/debian wheezy main contrib non-free > /etc/apt/sources.list
+/bin/echo deb http://192.168.0.199:9999/debian wheezy-updates main contrib non-free >> /etc/apt/sources.list
+/bin/echo deb http://192.168.0.199:9999/security wheezy/updates main contrib non-free >> /etc/apt/sources.list
